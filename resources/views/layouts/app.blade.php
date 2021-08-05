a184c315d9ca8c47b59cb69cb398ce18ea3daf03
cybert code <cybertcode@gmail.com>
2021-08-05 12:25:57 -0500

    agregamos el menú de navegación con alpine le dimos funcionalidad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,12 +22,13 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            <!-- importamos el componente de navegación de livewire -->
+            @livewire('navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
